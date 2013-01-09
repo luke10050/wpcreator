eac5d70ca34b5de2e3cdeed2fd4b2aac2502514f
Tristan <broadwaytat@gmail.com>
2013-01-09 15:42:47 +1100

    a test

---
+++ Desktop/test.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdafx.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <dirent.h>
+
+
+int main(int argc, char* argv[]) {
+	int name, dir, dur;
+	printf ("please enter a name for this slideshow");
+	scanf ("%d", &name);
+	printf ("please enter the directory the pictures are in");
+	scanf ("%d", &dir);
+	printf ("please enter a slide duration")
+	scanf ("%d", &dur);
+DIR * opendir (const char *dir);
+
+return 0;
+	
+
+
+
+
+ofstream outputFile(name+".xml");
