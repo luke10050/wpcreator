fc87e14fd974947e138edc72022afb32f24d8a78
Tristan <broadwaytat@gmail.com>
2013-01-10 17:29:28 +1100

    program working

--- Desktop/WORKS.cpp
+++ Desktop/WORKS.cpp
@@ -11,12 +11,14 @@
 
 using namespace std;
 
+int z;
+
 int main() {
 
 
 
-	int x, c, z, t; /* c stands for countdown */
-	x=0, c=10, z=0, t=5;
+	int x, c, t; /* c stands for countdown */
+	x=1, c=1, z=0, t=1;
 	string name, dir, dur;
 	string * tab;
 	vector<string> filename;
@@ -26,8 +28,10 @@ int main() {
 	cin >> name;
 	cout << "please enter the directory the pictures are located in: " << endl;
 	cin >> dir;
-	cout << "please enter the slide duration: " << endl;
+	cout << "please enter the slide duration in seconds: " << endl;
 	cin >> dur;
+	cout << "enter the number of files in the directory" << endl;
+	cin >> c;
 //add in the contents read and table listing here!
 string result;
        DIR *dp;
@@ -36,18 +40,18 @@ string result;
        if (dp != NULL)
          {
            while (ep = readdir (dp)){
-             cout << (ep->d_name) << endl;
-		
+		command = dir;
 		command.append(ep->d_name);
+		cout << command << endl;
 		filename.insert (filename.begin() + z, command);
+		command.clear();
+		cout << command << endl;
 		z=z+1;
 		}
            (void) closedir (dp);
          }
        else
          perror ("Couldn't open the directory");
-
-
 
 const string file = name + ".txt";
 
@@ -64,10 +68,11 @@ outputFile << "<second>00</second>" << endl;
 outputFile << "</starttime>" << endl; 
 outputFile << "<static>" << endl;
 outputFile << "<duration>" << dur << "</duration>" << endl;
+outputFile << "<file>" << filename[x] << "</file>" << endl;
 outputFile << "</static>" << endl;
 outputFile << "<transition>" << endl; 
 outputFile << "<duration>02</duration>" << endl;
-outputFile << "<from></from>" << endl;
+outputFile << "<from>" << filename[c] << "</from>" << endl;
 outputFile << "<to>"<< filename[x+1] << "</to>" << endl;
 outputFile << "</transition>" << endl;
 while(c != 0)
@@ -87,6 +92,19 @@ outputFile << "</transition>" << endl;
 x=x+1;
 c=c-1;
 }
+
+outputFile << "<static>" << endl;
+outputFile << "<duration>" << dur << "</duration>" << endl;
+outputFile << "<file>" << filename[x] << "</file>" << endl;
+outputFile << "</static>" << endl;
+
+
+outputFile << "<transition>" << endl; 
+outputFile << "<duration>02</duration>" << endl;
+outputFile << "<from>" << filename[x] << "</from>" << endl;
+outputFile << "<to>" << filename[t] << "</to>" << endl;
+outputFile << "</transition>" << endl;
+
 outputFile << "</background>" << endl;
 outputFile.close();
 cout << "Done!\n";
