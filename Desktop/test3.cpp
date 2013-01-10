43fd7e4a7f451f393b07f74b715c869c4e4808b1
Tristan <broadwaytat@gmail.com>
2013-01-10 13:34:35 +1100

    program a2

--- Desktop/test3.cpp
+++ Desktop/test3.cpp
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string>
+#include <string.h>
 #include <vector>
 
 using namespace std;
@@ -14,11 +15,10 @@ int main() {
 
 
 
-	int x, c; /* c stands for countdown */
-	x=0, c=2;
+	int x, c, z, t; /* c stands for countdown */
+	x=0, c=10, z=0, t=5;
 	string name, dir, dur;
-	string filename [3/*make this an output from counting the number of jpegs */] = {/* make program add .jpg names here */"5", "6"};
-
+	char * tab;
 	
 	cout << "please enter a name for this slideshow: " << endl;
 	cin >> name;
@@ -26,11 +26,23 @@ int main() {
 	cin >> dir;
 	cout << "please enter the slide duration: " << endl;
 	cin >> dur;
-
+string filename [20/*make this an output from counting the number of jpegs */];
 //add in the contents read and table listing here!
-
-DIR *opendir(const char *dir);
-
+string result;
+       DIR *dp;
+       struct dirent *ep;
+ 	dp = opendir ("./");
+       if (dp != NULL)
+         {
+           while (ep = readdir (dp)){
+             cout << (ep->d_name) << endl;
+		strcpy((ep->d_name), tab);
+		z=z+1;
+		}
+           (void) closedir (dp);
+         }
+       else
+         perror ("Couldn't open the directory");
 
 
 
@@ -55,7 +67,7 @@ outputFile << "<duration>02</duration>" << endl;
 outputFile << "<from></from>" << endl;
 outputFile << "<to>"<< filename[x+1] << "</to>" << endl;
 outputFile << "</transition>" << endl;
-if(c != 0)
+while(c != 0)
 {
 outputFile << "<static>" << endl;
 outputFile << "<duration>" << dur << "</duration>" << endl;
@@ -65,7 +77,7 @@ outputFile << "</static>" << endl;
 
 outputFile << "<transition>" << endl; 
 outputFile << "<duration>02</duration>" << endl;
-outputFile << "<from>" << filename[x-1] << "</from>" << endl;
+outputFile << "<from>" << filename[x] << "</from>" << endl;
 outputFile << "<to>" << filename[x+1] << "</to>" << endl;
 outputFile << "</transition>" << endl;
 
@@ -75,7 +87,6 @@ c=c-1;
 outputFile << "</background>" << endl;
 outputFile.close();
 cout << "Done!\n";
-
 return 0;
 }
 
